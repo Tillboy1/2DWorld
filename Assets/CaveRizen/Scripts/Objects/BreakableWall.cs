719a0326fafc712415f01a43a886f06125145702
Tillboy1 <keiranm9@gmail.com>
2026-02-20 21:32:16 +0000

    brakable

---
+++ Assets/CaveRizen/Scripts/Objects/BreakableWall.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BreakableWall : MonoBehaviour
+{
+    int CurrentHealth = 3;
+    int MaxHealth = 3;
+    public GameObject NextRoomBreakable;
+    public GameObject[] Doorways;
+
+    public void TakeDamage()
+    {
+        CurrentHealth--;
+        if(CurrentHealth <= 0)
+        {
+            BreakWall();
+        }
+    }
+    public void BreakWall()
+    {
+        if(NextRoomBreakable != null)
+        {
+            Destroy(NextRoomBreakable);
+        }
+        Destroy(this.gameObject);
+        if (Doorways[0] != null)
+        {
+            Doorways[0].SetActive(true);
+            Doorways[1].SetActive(true);
+        }
+    }
+}
